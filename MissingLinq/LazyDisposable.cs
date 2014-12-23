4a8d48db80c545c4360c59f775516bcc7e85846b
Jay Otterbein <jay.otterbein@headspring.com>
2014-12-23 10:27:14 -0600

    added LazyDisposable and tests

---
+++ MissingLinq/LazyDisposable.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Threading;
+
+namespace MissingLinq
+{
+    public class LazyDisposable<T>
+        where T : IDisposable
+    {
+        private readonly Func<Lazy<T>> _lazyFactory;
+        private Lazy<T> _lazy;
+
+        public LazyDisposable(Func<T> valueFactory)
+        {
+            _lazyFactory = () => new Lazy<T>(valueFactory);
+            _lazy = _lazyFactory();
+        }
+
+        public LazyDisposable(Func<T> valueFactory, LazyThreadSafetyMode threadSafetyMode)
+        {
+            _lazyFactory = () => new Lazy<T>(valueFactory, threadSafetyMode);
+            _lazy = _lazyFactory();
+        }
+
+        public LazyDisposable(Func<T> valueFactory, bool isThreadSafe)
+        {
+            _lazyFactory = () => new Lazy<T>(valueFactory, isThreadSafe);
+            _lazy = _lazyFactory();
+        }
+
+        public LazyDisposable()
+        {
+            _lazyFactory = () => new Lazy<T>();
+            _lazy = _lazyFactory();
+        }
+
+        public LazyDisposable(LazyThreadSafetyMode threadSafetyMode)
+        {
+            _lazyFactory = () => new Lazy<T>(threadSafetyMode);
+            _lazy = _lazyFactory();
+        }
+
+        public LazyDisposable(bool isThreadSafe)
+        {
+            _lazyFactory = () => new Lazy<T>(isThreadSafe);
+            _lazy = _lazyFactory();
+        }
+
+        public T Value
+        {
+            get { return _lazy.Value; }
+        }
+
+        public bool IsValueCreated
+        {
+            get { return _lazy.IsValueCreated; }
+        }
+
+        /// <summary>
+        ///     Disposes of the inner value if it exists.
+        ///     This can be called multiple times without a performance penalty.
+        ///     This will still allow <see cref="Value" /> to be called and a new value to be generated when needed.
+        /// </summary>
+        public void DisposeValue()
+        {
+            if (_lazy.IsValueCreated)
+            {
+                _lazy.Value.Dispose();
+                _lazy = _lazyFactory();
+            }
+        }
+    }
+}
---
+++ MissingLinqTests/LazyDisposableTests.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using MissingLinq;
+using NUnit.Framework;
+
+namespace MissingLinqTests
+{
+    [TestFixture]
+    public class LazyDisposableTests
+    {
+        [Test]
+        public void Factory_not_called_for_dispose()
+        {
+            var factoryCalled = 0;
+            Func<Model> modelFactory = () =>
+            {
+                factoryCalled++;
+                return new Model();
+            };
+
+            var lazyDisposable = new LazyDisposable<Model>(modelFactory);
+            lazyDisposable.DisposeValue();
+            lazyDisposable.DisposeValue();
+            lazyDisposable.DisposeValue();
+            lazyDisposable.DisposeValue();
+            Assert.That(factoryCalled, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Factory_called_once_for_repeated_value_access()
+        {
+            var factoryCalled = 0;
+            Func<Model> modelFactory = () =>
+            {
+                factoryCalled++;
+                return new Model();
+            };
+
+            var lazyDisposable = new LazyDisposable<Model>(modelFactory);
+            for (var i = 0; i < 10; i++)
+            {
+                var temp = lazyDisposable.Value;
+                Assert.That(temp, Is.Not.Null);
+            }
+            Assert.That(factoryCalled, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Factory_called_for_every_value_access_after_dispose()
+        {
+            var factoryCalled = 0;
+            Func<Model> modelFactory = () =>
+            {
+                factoryCalled++;
+                return new Model();
+            };
+
+            var lazyDisposable = new LazyDisposable<Model>(modelFactory);
+            for (var i = 0; i < 10; i++)
+            {
+                var temp = lazyDisposable.Value;
+                Assert.That(temp, Is.Not.Null);
+                lazyDisposable.DisposeValue();
+            }
+
+            Assert.That(factoryCalled, Is.EqualTo(10));
+        }
+
+        [Test]
+        public void Dispose_calls_underlaying_dispose_only_once()
+        {
+            var factoryCalled = 0;
+            Func<Model> modelFactory = () =>
+            {
+                factoryCalled++;
+                return new Model();
+            };
+
+            var lazyDisposable = new LazyDisposable<Model>(modelFactory);
+            var models = new List<Model>();
+            for (var i = 0; i < 10; i++)
+            {
+                var lazyLoadedModel = lazyDisposable.Value;
+                models.Add(lazyLoadedModel);
+                lazyDisposable.DisposeValue();
+                lazyDisposable.DisposeValue();
+                lazyDisposable.DisposeValue();
+                lazyDisposable.DisposeValue();
+            }
+
+            Assert.That(models.Count, Is.EqualTo(10), "models not added to testing array");
+            foreach (var model in models)
+            {
+                Assert.That(model.DisposeCallCount, Is.EqualTo(1));
+            }
+        }
+
+        private class Model : IDisposable
+        {
+            public int DisposeCallCount;
+
+            public void Dispose()
+            {
+                DisposeCallCount++;
+            }
+        }
+    }
+}
